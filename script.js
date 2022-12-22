eb0dbc489e7fa7dc5f8eef2ceaec8ab056f3c1b2
Treyceratops <treytowers@yahoo.com>
2022-12-21 23:00:18 -0800

    get rid of scroll, can move any direction without a tail

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ function checkLeafCollision() {
 function keyDown(event) {
 	// up
 	if (event.keyCode == 38) {
-		if (yDirection == 1) {
+		if (caterpillarParts.length > 0 && yDirection == 1) {
 			return;
 		}
 		yDirection = -1;
@@ -180,7 +180,7 @@ function keyDown(event) {
 
 	// down
 	if (event.keyCode == 40) {
-		if (yDirection == -1) {
+		if (caterpillarParts.length > 0 && yDirection == -1) {
 			return;
 		}
 		yDirection = 1;
@@ -189,7 +189,7 @@ function keyDown(event) {
 
 	// left
 	if (event.keyCode == 37) {
-		if (xDirection == 1) {
+		if (caterpillarParts.length > 0 && xDirection == 1) {
 			return;
 		}
 		yDirection = 0;
@@ -198,7 +198,7 @@ function keyDown(event) {
 
 	// right
 	if (event.keyCode == 39) {
-		if (xDirection == -1) {
+		if (caterpillarParts.length > 0 && xDirection == -1) {
 			return;
 		}
 		yDirection = 0;
